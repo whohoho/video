aa9b8632b4b798dbba1479d0a816b2d9af5b6246
hark <hark@puscii.nl>
2020-04-03 05:16:05 +0200

    wtf.js is a working readablestream with video

--- http/janus-sfu/audiorec.js
+++ http/janus-sfu/audiorec.js
@@ -1,6 +1,6 @@
 
 let chunks = [];
-const AUDIO_CONSTRAINTS = {audio: true};
+const AUDIO_CONSTRAINTS = {audio: true, video: true};
 const AUDIO_MIME = 'audio/webm;codecs=opus';
 const VIDEO_CONSTRAINTS = {video: true};
 const VIDEO_MIME = 'video/webm;codecs=vp8';
@@ -11,23 +11,40 @@ const AUDIO_RECOPT = {
         //audioBitsPerSecond :  256000,
 	      //videoBitsPerSecond : 2500000,
 	      //bitsPerSecond:       2628000,
-	      mimeType : AUDIO_MIME,
+//	      mimeType : AUDIO_MIME,
         timeslice: 100,
  	    };
 
 const VIDEO_RECOPT = { audioBitsPerSecond :  256000,
 	      //videoBitsPerSecond : 2500000,
 	      //bitsPerSecond:       2628000,
-	      mimeType : VIDEO_MIME,
+//	      mimeType : VIDEO_MIME,
         timeslice: 10,
  	    };
 
 
+  function randomChars() {
+    let string = "";
+    let choices = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+
+    for (let i = 0; i < 8; i++) {
+      string += choices.charAt(Math.floor(Math.random() * choices.length));
+    }
+    return string;
+  }
+
 function makeMediaStream(inputstream, recopts) {
 	var recorder;
- 
+  console.log(inputstream);
+ if (inputstream instanceof MediaStream) {
+   console.log('inputstream == MediaStream');
+ }
+
+
   recorder = new MediaRecorder(inputstream, recopts);
-  console.log("new mediastream created");
+
+  console.log("new mediarecorder created: ");
+  console.log(recorder);
 
   function la (event){
     controller.enqueue(event.data);
@@ -37,10 +54,53 @@ function makeMediaStream(inputstream, recopts) {
 
   return new ReadableStream({
     start(controller) {
+      media = getMedia({video: true});
+      interval = setInterval(() => {
+        let string = randomChars();
+
+        // Add the string to the stream
+        controller.enqueue(string);
+
+        // show it on the screen
+        let listItem = document.createElement('li');
+        listItem.textContent = string;
+        document.body.appendChild(listItem);
+      }, 1000);
+
+      /*
+      button.addEventListener('click', function() {
+        clearInterval(interval);
+        readStream();
+        controller.close();
+      })
+      */
+    },
+    pull(controller) {
+      // We don't really need a pull in this example
+    },
+    cancel() {
+      // This is called if the reader cancels,
+      // so we should stop generating strings
+      clearInterval(interval);
+    }
+  });
+
+/*
+  return new ReadableStream({
+    pull(controller) {
+      controller.enqueue('wtf');
+      console.log('pulled');
+  //    controller.enqueue(recorder.requestData());
+      recorder.start();
+    },
+    start(controller) {
       console.log("stream has started");
       console.log(recorder.state);
+      
       recorder.ondataavailable = function (ev) {
-        controller.enqueue(ev.data);
+          controller.enqueue('wtf');
+
+        //controller.enqueue(ev.data);
         console.log('rec data');
         console.log(ev.data);
       };
@@ -50,14 +110,17 @@ function makeMediaStream(inputstream, recopts) {
       recorder.start();
 //      console.log(recorder.ondataAvailable);
       console.log(recorder.state);
-//      console.log(recorder.requestData());
-
+      console.log("mediarecorder started: ");
+      console.log(recorder);
+ //     console.log(recorder.requestData());
     },
 
     cancel() {
+      console.log('stream cancelled');
       recorder.stop();
     }
   });
+  */
 }
 
 
@@ -78,31 +141,79 @@ function insertPlayer (stream) {
     mediaEl.autoplay = true;
     mediaEl.play = true;
 
-//    mediaEl.srcObject = stream;
+    mediaEl.srcObject = stream;
     //mediaEl.src = stream;
 
     document.body.appendChild(mediaEl);
+/*  let response = new Response(stream);
+  (response.blob())
+  .then(blob => URL.createObjectURL(blob))
+  .then(url => mediaEl.src = url )
+  .catch(err => console.error(err));
+  console.log("now the audio should play");
+*/
+}
+function insertBlobPlayer (stream) {
+    var mediaEl = document.createElement("video");
+    mediaEl.controls = true;
+    mediaEl.autoplay = true;
+    mediaEl.play = true;
+    document.body.appendChild(mediaEl);
+
+
   let response = new Response(stream);
   (response.blob())
   .then(blob => URL.createObjectURL(blob))
   .then(url => mediaEl.src = url )
   .catch(err => console.error(err));
   console.log("now the audio should play");
+  console.log(stream);
+  console.log(stream);
 
 }
 
+
+
+
+async function init(constraints) {
+}
+
+
 async function test () {
 
-  let audioin = await getMedia(AUDIO_CONSTRAINTS);
-  let audiostream = await makeMediaStream(audioin, AUDIO_RECOPT);
-  console.log("we have audio, trying to play");
-  insertPlayer(audiostream);
+  try {
+    const audioin = await navigator.mediaDevices.getUserMedia(AUDIO_CONSTRAINTS);
+    console.log('getUserMedia() got stream:', audioin);
+    insertPlayer(audioin);
+    let audiostream = await makeMediaStream(audioin, AUDIO_RECOPT);
+      const reader = audiostream.getReader();
+      reader.read().then(function processText({ done, value }) {
+        console.log('value of stream read',value);
+    // Result objects contain two properties:
+    // done  - true if the stream has already given you all its data.
+    // value - some data. Always undefined when done is true.
+    if (done) {
+      console.log("Stream complete");
+      return;
+    }
+      });
+    console.log("we have audio, trying to play");
+    //insertBlobPlayer(audiostream);
 
+  } catch (e) {
+    console.error('navigator.getUserMedia error:', e);
+    //errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+  }
+
+ /* 
   let videoin = await getMedia(VIDEO_CONSTRAINTS);
+  insertPlayer(videoin);
+
   let videostream = await makeMediaStream(videoin, VIDEO_RECOPT);
   console.log("we have video, trying to play");
   insertPlayer(videostream);
-/*
+*/
+  /*
   let response = new Response(stream2);
 
   (response.blob())
