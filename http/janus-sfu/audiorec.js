05c60125d59c0bfc3f20a241a9d0184cf61a8704
hark <hark@puscii.nl>
2020-04-03 02:49:01 +0200

    bak

--- http/janus-sfu/audiorec.js
+++ http/janus-sfu/audiorec.js
@@ -2,24 +2,55 @@
 let chunks = [];
 const AUDIO_CONSTRAINTS = {audio: true};
 const AUDIO_MIME = 'audio/webm;codecs=opus';
+const VIDEO_CONSTRAINTS = {audio: true};
+const VIDEO_MIME = 'video/webm;codecs=vp8';
+
+
 const BUF_SIZE = 1;
 const AUDIO_RECOPT = { audioBitsPerSecond :  256000,
 	      videoBitsPerSecond : 2500000,
 	      bitsPerSecond:       2628000,
 	      mimeType : AUDIO_MIME,
+        timeslice: 10,
+	    };
+const VIDEO_RECOPT = { audioBitsPerSecond :  256000,
+	      videoBitsPerSecond : 2500000,
+	      bitsPerSecond:       2628000,
+	      mimeType : VIDEO_MIME,
+        timeslice: 10,
 	    };
 
-function makeAudioStream(mediastream) {
+
+function makeMediaStream(inputstream, recopts) {
 	var recorder;
  
-  recorder = new MediaRecorder(mediastream, AUDIO_RECOPT);
-  
+  recorder = new MediaRecorder(inputstream, recopts);
+  console.log("new mediastream created");
+
+  function la (event){
+    controller.enqueue(event.data);
+    console.log(event.data);
+    return data
+  }
 
   return new ReadableStream({
     start(controller) {
-      recorder.ondataavailable = event => controller.enqueue(event.data);
+      console.log("stream has started");
+      console.log(recorder.state);
+
+//      recorder.ondataavailable = event => la(event);
+      recorder.ondataavailable = function (ev) {
+        controller.enqueue(ev.data);
+        console.log(ev.data);
+      };
+
       recorder.onclose = () => controller.close();
       recorder.onerror = () => controller.error(new Error("audiorecorder error"));
+      recorder.start();
+      console.log(recorder.ondataAvailable);
+      console.log(recorder.state);
+      console.log(recorder.requestData());
+
     },
 
     cancel() {
@@ -32,37 +63,46 @@ function makeAudioStream(mediastream) {
 async function getMedia(constraints) {
   let stream = null;
   try {
-    stream = await navigator.mediaDevices.getUserMedia(AUDIO_CONSTRAINTS);
+    stream = await navigator.mediaDevices.getUserMedia(constraints);
     return stream;
   } catch (err) { console.log("getmediastream failed"); } 
-  console.log("getUserMedia() success, stream created, initializing MediaRecorder");  
+  console.log("getUserMedia() success, stream created, initializing MediaRecorder" + constraints);  
 }; 
 
 function insertPlayer (stream) {
 
+
     var mediaEl = document.createElement("audio");
-//    mediaEl.className = 'media-' + otherId;
     mediaEl.controls = true;
     mediaEl.autoplay = true;
-    mediaEl.srcObject = stream;
+//    mediaEl.srcObject = stream;
+    //mediaEl.src = stream;
+
     document.body.appendChild(mediaEl);
+  let response = new Response(stream);
+  (response.blob())
+  .then(blob => URL.createObjectURL(blob))
+  .then(url => mediaEl.src = url )
+  .catch(err => console.error(err));
+  console.log("now the audio should play");
 
 }
 
 async function test () {
 
-  let stream = await getMedia(AUDIO_CONSTRAINTS);
-
-  let audioelement = document.getElementById("testaudio")
-  let stream2 = await makeAudioStream(stream);
-  insertPlayer(stream);
+  let audioin = await getMedia(AUDIO_CONSTRAINTS);
+  let audiostream = await makeMediaStream(audioin, AUDIO_RECOPT);
   console.log("we have audio, trying to play");
+  insertPlayer(audiostream);
+
+  /*
   let response = new Response(stream2);
+
   (response.blob())
   .then(blob => URL.createObjectURL(blob))
   .then(url => audioelement.src = url )
   .catch(err => console.error(err));
-
+*/
 
 
 };
